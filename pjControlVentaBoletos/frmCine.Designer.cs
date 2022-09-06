bc6409b42eaa6ecaa945fb476ffdcb05a266d0a8
Eduardo Ezequiel Martinez Narvaez <eduardo.martinez96u@std.uni.edu.ni>
2022-09-06 09:31:41 -0600

    Agregar archivos de proyecto.

---
+++ pjControlVentaBoletos/frmCine.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace pjControlVentaBoletos
+{
+    partial class frmCine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRazon = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cboEdad = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lvRegistro = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
+            this.btnEstadisticas = new System.Windows.Forms.Button();
+            this.lvEstadisticas = new System.Windows.Forms.ListView();
+            this.columnHeader7 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader8 = new System.Windows.Forms.ColumnHeader();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(124, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(460, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE VENTA DE BOLETOS - CORPORATIVO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RAZÓN SOCIAL";
+            // 
+            // txtRazon
+            // 
+            this.txtRazon.Location = new System.Drawing.Point(27, 60);
+            this.txtRazon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtRazon.Name = "txtRazon";
+            this.txtRazon.Size = new System.Drawing.Size(547, 23);
+            this.txtRazon.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.lblCategoria);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblPrecio);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.cboEdad);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(28, 93);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(633, 94);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "REGISTRO DE BOLETOS";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(510, 41);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(82, 22);
+            this.btnRegistrar.TabIndex = 8;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(345, 42);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(110, 23);
+            this.txtCantidad.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(345, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 15);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "CANTIDAD";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(250, 46);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(38, 15);
+            this.lblCategoria.TabIndex = 5;
+            this.lblCategoria.Text = "label7";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(250, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "CATEGORÍA";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(175, 46);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(38, 15);
+            this.lblPrecio.TabIndex = 3;
+            this.lblPrecio.Text = "label5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(175, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "PRECIO";
+            // 
+            // cboEdad
+            // 
+            this.cboEdad.FormattingEnabled = true;
+            this.cboEdad.Items.AddRange(new object[] {
+            "03 - 12",
+            "13 - 59",
+            ">60"});
+            this.cboEdad.Location = new System.Drawing.Point(7, 45);
+            this.cboEdad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboEdad.Name = "cboEdad";
+            this.cboEdad.Size = new System.Drawing.Size(133, 23);
+            this.cboEdad.TabIndex = 1;
+            this.cboEdad.SelectedIndexChanged += new System.EventHandler(this.cboEdad_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "EDAD";
+            // 
+            // lvRegistro
+            // 
+            this.lvRegistro.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvRegistro.GridLines = true;
+            this.lvRegistro.Location = new System.Drawing.Point(30, 196);
+            this.lvRegistro.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lvRegistro.Name = "lvRegistro";
+            this.lvRegistro.Size = new System.Drawing.Size(631, 114);
+            this.lvRegistro.TabIndex = 4;
+            this.lvRegistro.UseCompatibleStateImageBehavior = false;
+            this.lvRegistro.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "CATEGORÍA";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PRECIO UNI.";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "CANT.";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "SUBTOTAL";
+            this.columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "DESCUENTO";
+            this.columnHeader5.Width = 120;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "IMPORTE";
+            this.columnHeader6.Width = 80;
+            // 
+            // btnEstadisticas
+            // 
+            this.btnEstadisticas.Location = new System.Drawing.Point(27, 314);
+            this.btnEstadisticas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnEstadisticas.Name = "btnEstadisticas";
+            this.btnEstadisticas.Size = new System.Drawing.Size(168, 22);
+            this.btnEstadisticas.TabIndex = 5;
+            this.btnEstadisticas.Text = "MOSTRAR ESTADÍSTICAS";
+            this.btnEstadisticas.UseVisualStyleBackColor = true;
+            this.btnEstadisticas.Click += new System.EventHandler(this.btnEstadisticas_Click);
+            // 
+            // lvEstadisticas
+            // 
+            this.lvEstadisticas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader7,
+            this.columnHeader8});
+            this.lvEstadisticas.GridLines = true;
+            this.lvEstadisticas.Location = new System.Drawing.Point(30, 340);
+            this.lvEstadisticas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lvEstadisticas.Name = "lvEstadisticas";
+            this.lvEstadisticas.Size = new System.Drawing.Size(631, 167);
+            this.lvEstadisticas.TabIndex = 6;
+            this.lvEstadisticas.UseCompatibleStateImageBehavior = false;
+            this.lvEstadisticas.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "CONDICIÓN";
+            this.columnHeader7.Width = 400;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "VALOR";
+            this.columnHeader8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader8.Width = 100;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(30, 510);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(82, 22);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmCine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 541);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lvEstadisticas);
+            this.Controls.Add(this.btnEstadisticas);
+            this.Controls.Add(this.lvRegistro);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtRazon);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmCine";
+            this.Text = "Control de venta de boletos - Corporativo";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtRazon;
+        private GroupBox groupBox1;
+        private Button btnRegistrar;
+        private TextBox txtCantidad;
+        private Label label8;
+        private Label lblCategoria;
+        private Label label6;
+        private Label lblPrecio;
+        private Label label4;
+        private ComboBox cboEdad;
+        private Label label3;
+        private ListView lvRegistro;
+        private Button btnEstadisticas;
+        private ListView lvEstadisticas;
+        private Button btnSalir;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+    }
+}
---
+++ pjControlVentaBoletos/frmCine.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pjControlVentaBoletos
+{
+    public partial class frmCine : Form
+    {
+        double precio = 0;
+        string categoria = "";
+
+        public frmCine()
+        {
+            InitializeComponent();
+        }
+
+        private void cboEdad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Capturando la edad seleccionada
+            int edad = cboEdad.SelectedIndex;
+
+            // Asignando el precio y categoría según la edad seleccionada
+            switch(edad)
+            {
+                case 0: precio = 10; categoria = "Niño"; break; 
+                case 1: precio = 25; categoria = "Adulto"; break; 
+                case 2: precio = 15; categoria = "Tercera Edad"; break; 
+            }
+
+            // Mostrando el precio y la categoría
+            lblPrecio.Text = precio.ToString("C");
+            lblCategoria.Text = categoria;
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // Capturando los datos necesarios
+            categoria = lblCategoria.Text;
+            int cantidad = int.Parse(txtCantidad.Text);
+
+            // Realizando los cálculos
+            double subtotal = precio * cantidad;
+            double descuento = 0;
+
+            switch(categoria)
+            {
+                case "Niño": descuento = 20.0 / 100 * subtotal; break;
+                case "Adulto": descuento = 5.0 / 100 * subtotal; break;
+                case "Tercera Edad": descuento = 10.0 / 100 * subtotal; break;
+            }
+            double importe = subtotal - descuento;
+
+            // Imprimir en la lista
+            ListViewItem fila = new ListViewItem(categoria);
+            fila.SubItems.Add(precio.ToString("0.00"));
+            fila.SubItems.Add(cantidad.ToString());
+            fila.SubItems.Add(subtotal.ToString("0.00"));
+            fila.SubItems.Add(descuento.ToString("0.00"));
+            fila.SubItems.Add(importe.ToString("0.00"));
+            lvRegistro.Items.Add(fila);
+
+            lvEstadisticas.Items.Clear();
+        }
+
+        private void btnEstadisticas_Click(object sender, EventArgs e)
+        {
+            lvEstadisticas.Items.Clear();
+
+            // Hallar el monto total sin descuento
+            double tSubtotal = 0;
+            int i;
+            for(i = 0; i < lvRegistro.Items.Count; i++)
+            {
+                tSubtotal += double.Parse(lvRegistro.Items[i].SubItems[3].Text);
+            }
+
+            // Hallar el monto total que la empresa no percibe
+            // los decuentos realizados
+            double tDescuento = 0;
+            i = 0;
+            while(i < lvRegistro.Items.Count)
+            {
+                tDescuento += double.Parse(lvRegistro.Items[i].SubItems[4].Text);
+                i++;
+            }
+
+            // Hallar el monto total acumulado por categoría
+            double aNiño = 0, aAdulto = 0, aTerceraEdad = 0;
+            i = 0;
+            do
+            {
+                string categoria = lvRegistro.Items[i].SubItems[0].Text;
+                switch (categoria)
+                {
+                    case "Niño":
+                        aNiño += double.Parse(lvRegistro.Items[i].SubItems[5].Text);
+                        break;
+                    case "Adulto":
+                        aAdulto += double.Parse(lvRegistro.Items[i].SubItems[5].Text);
+                        break;
+                    case "Tercera Edad":
+                        aTerceraEdad += double.Parse(lvRegistro.Items[i].SubItems[5].Text);
+                        break;
+                }
+                i++;
+            } while(i < lvRegistro.Items.Count);
+
+            // Imprimiendo estadísticas
+            lvEstadisticas.Items.Clear();
+            string[] elementosFila = new string[2];
+            ListViewItem row;
+
+            elementosFila[0] = "Monto total sin descuento";
+            elementosFila[1] = tSubtotal.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Monto total que la empresa no percibe";
+            elementosFila[1] = tDescuento.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Monto acumulado por categoría Niño";
+            elementosFila[1] = aNiño.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Monto acumulado por categoría Adulto";
+            elementosFila[1] = aAdulto.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Monto acumulado por categoría Tercera Edad";
+            elementosFila[1] = aTerceraEdad.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("¿Está seguro de salir?",
+                                             "Cine",
+                                             MessageBoxButtons.YesNo,
+                                             MessageBoxIcon.Question);
+            if(r == DialogResult.Yes)
+                this.Close();
+        }
+    }
+}
